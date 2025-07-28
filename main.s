c110730a9b1fbcd1ccad1d48b8012d763d5573a0
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-07-28 19:54:19 +0200

    Remove old invalid license bits

--- main.s
+++ main.s
@@ -5,13 +5,6 @@
 ; published by the Free Software Foundation, either version 3 of the
 ; License, or (at your option) any later version.
 ;
-; As an added restriction, if you make the program available for
-; third parties to use on hardware you own (or co-own, lease, rent,
-; or otherwise control,) such as public gaming cabinets (whether or
-; not in a gaming arcade, whether or not coin-operated or otherwise
-; for a fee,) the conditions of section 13 will apply even if no
-; network is involved.
-;
 ; This program is distributed in the hope that it will be useful,
 ; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
