e1a54071e14cbdf397afbaf629e42ef555c37da0
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-08-01 17:08:35 +0200

    Add comments to code that handles IKBD queue

--- game.s
+++ game.s
@@ -31,7 +31,7 @@
 
 DrawStart:
 DrawLoop:
-	addq.l #1, time_render.l				; Count number of rendered frames
+	addq.l #1, time_render.l			; Count number of rendered frames
 
 ; ###########################
 ; ##                       ##
@@ -39,10 +39,9 @@ DrawLoop:
 ; ##                       ##
 ; ###########################
 
-
-	lea.l acia_rx_buffer.l, a0
-
-	move.w acia_rx_woffset.l, d7
+DrawProcessInput:
+	lea.l acia_rx_buffer.l, a0			; a0: base address for the buffer
+	move.w acia_rx_woffset.l, d7		; d7: offset until which to read
 
 ; **********************************
 ; * Separate combined read / x / y *
@@ -53,87 +52,98 @@ DrawLoop:
 	move.l d0, d6
 	swap.w d6
 	lsr.w #5, d6
-	andi.w #$3ff << 1, d6					; offset from which to read
+	andi.w #$3ff << 1, d6				; d6: offset from which to read, on event boundary
 
 ; bottom 11 bits: x coordinate
 	move.l d0, d3
-	andi.w #$7ff, d3						; mouse x position at beginning of processing
+	andi.w #$7ff, d3					; d3: mouse x position at beginning of processing
 
 ; middle 11 bits: y coordinate
 	move.l d0, d4
 	lsl.l #5, d4
 	swap.w d4
-	andi.w #$7ff, d4						; mouse y position at beginning of processing
+	andi.w #$7ff, d4					; d4: mouse y position at beginning of processing
 
-.NextPacket:
-	cmp.w d6, d7
-	beq.w .all_read.l
-	move.w d6, d5
+.NextEvent:
+	cmp.w d6, d7						; End of available data?
+	beq.w .all_read.l					; Yes: all done
+	move.w d6, d5						; d5: current read offset, including partial events
 
-	move.b 0(a0, d5.w), d0
+	move.b 0(a0, d5.w), d0				; Read first byte from the event
 
-	addq.w #2, d5
-	andi.w #IKBD_QUEUE_SIZE - 2, d5			; Wrap within buffer
+	addq.w #2, d5						; Advance read location to next byte
+	andi.w #IKBD_QUEUE_SIZE - 2, d5		; Wrap within buffer
 
-	cmpi.b #$fe, d0
-	blo.s .NotJoy.l
-	cmp.w d5, d7
-	beq.w .all_read.l
-	move.b 0(a0, d5.w), d1
-	addq.w #2, d5
-	bra.w .PacketDone
+.CheckJoystick:
+	cmpi.b #$fe, d0						; Check if this is a joystick event (fe-ff)
+	blo.s .CheckMouse.l					; No: check for another packet type
 
-.NotJoy:
-	cmpi.b #$f8, d0
-	blo.s .NotMouse.l
+	cmp.w d5, d7						; End of available data?
+	beq.w .all_read.l					; Yes: all done
 
-	cmp.w d5, d7
-	beq.w .all_read.l
-	move.b 0(a0, d5.w), d1
-	addq.w #2, d5
-	andi.w #IKBD_QUEUE_SIZE - 2, d5			; Wrap within buffer
+	; Read extra joystick byte here
 
-	cmp.w d5, d7
-	beq.w .all_read.l
-	move.b 0(a0, d5.w), d2
-	addq.w #2, d5
+	addq.w #2, d5						; Advance read location to next byte
+	andi.w #IKBD_QUEUE_SIZE - 2, d5		; Wrap within buffer
 
-	andi.w #$3, d0
+	bra.w .EventDone					; This event is done, wrap up and keep processing
+
+.CheckMouse:
+	cmpi.b #$f8, d0						; Check if this is a mouse event (f8-fb)
+	blo.s .DoKeyboard.l					; No: go to next possible event type
+
+	cmp.w d5, d7						; End of available data?
+	beq.w .all_read.l					; Yes: all done
+
+	move.b 0(a0, d5.w), d1				; d1: first mouse extra byte (delta x)
+
+	addq.w #2, d5						; Advance read location to next byte
+	andi.w #IKBD_QUEUE_SIZE - 2, d5		; Wrap within buffer
+
+	cmp.w d5, d7						; End of available data?
+	beq.w .all_read.l					; Yes: all done
+
+	move.b 0(a0, d5.w), d2				; d2: second mouse extra byte (delta y)
+
+	addq.w #2, d5						; Advance read location to next byte
+	andi.w #IKBD_QUEUE_SIZE - 2, d5		; Wrap within buffer
+
+	andi.w #$3, d0						; Top 2 bits of first byte are mouse buttons
 	move.w d0, mouse_buttons
 
-	ext.w d1
-	add.w d3, d1
-	bpl.s .OkX1
+	ext.w d1							; Sign-extend delta-x to word
+	add.w d3, d1						; Update x accordingly
+	bpl.s .OkX1							; Constrain to x >= 0
 	moveq.l #0, d1
 	bra.s .OkX2
 .OkX1:
 	cmpi.w #640, d1
-	blt.s .OkX2
+	blt.s .OkX2							; Constrain to x <= 639
 	move.w #639, d1
 .OkX2:
 	move.w d1, d3
 
-	ext.w d2
-	add.w d4, d2
-	bpl.s .OkY1
+	ext.w d2							; Sign-extend delta-y to word
+	add.w d4, d2						; Update y accordingly
+	bpl.s .OkY1							; Constrain to y >= 0
 	moveq.l #0, d2
 	bra.s .OkY2
 .OkY1:
 	cmpi.w #200, d2
-	blt.s .OkY2
+	blt.s .OkY2							; Constrain to y <= 199
 	move.w #199, d2
 .OkY2:
 	move.w d2, d4
 
-	bra.s .PacketDone.l
+	bra.s .EventDone.l					; This event is done, wrap up and keep processing
 
-.NotMouse:
-	moveq.l #0, d1
+.DoKeyboard:
+	moveq.l #0, d1						; At this point we're handling a keyboard event
 	move.b d0, d1
-	andi.b #$7f, d1
+	andi.b #$7f, d1						; Get the key number (bottom 7 bits of event)
 	move.l d1, d2
-	andi.w #7, d1
-	lsr.w #3, d2
+	andi.w #7, d1						; Key number modulo 8
+	lsr.w #3, d2						; Key number / 8
 	lea.l keyboard_state.l, a3
 	adda.w d2, a3
 	moveq.l #0, d2
@@ -147,10 +157,10 @@ DrawLoop:
 	and.b d2, (a3)
 .KeyDone:
 
-.PacketDone:
+.EventDone:
 	move.w d5, d6
 	andi.w #IKBD_QUEUE_SIZE - 2, d6			; Wrap within buffer
-	bra.w .NextPacket
+	bra.w .NextEvent
 .all_read:
 
 	lsl.w #5, d6
