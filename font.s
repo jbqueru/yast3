be83118558e8b765ca094443fec4faa656b3e11a
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-08-05 11:15:13 +0300

    Move font to separate file

---
+++ font.s
@@ -0,0 +1,112 @@
+; Copyright 2025 Jean-Baptiste M. "JBQ" "Djaybee" Queru
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as
+; published by the Free Software Foundation, either version 3 of the
+; License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License
+; along with this program. If not, see <https://www.gnu.org/licenses/>.
+;
+; SPDX-License-Identifier: AGPL-3.0-or-later
+
+; See main.s for more information
+
+	.data
+
+font:
+
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %00000000
+
+	dc.b %00011000
+	dc.b %00111000
+	dc.b %00011000
+	dc.b %00011000
+	dc.b %00011000
+	dc.b %00011000
+	dc.b %00111100
+	dc.b %00000000
+
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %00000110
+	dc.b %00111100
+	dc.b %01100000
+	dc.b %01100000
+	dc.b %01111110
+	dc.b %00000000
+
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %00000110
+	dc.b %00011100
+	dc.b %00000110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %00000000
+
+	dc.b %01100000
+	dc.b %01100000
+	dc.b %01100000
+	dc.b %01101100
+	dc.b %01111110
+	dc.b %00001100
+	dc.b %00001100
+	dc.b %00000000
+
+	dc.b %01111110
+	dc.b %01100000
+	dc.b %01111100
+	dc.b %00000110
+	dc.b %00000110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %00000000
+
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %01100000
+	dc.b %01111100
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %00000000
+
+	dc.b %01111110
+	dc.b %00000110
+	dc.b %00001100
+	dc.b %00011000
+	dc.b %00011000
+	dc.b %00011000
+	dc.b %00011000
+	dc.b %00000000
+
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %00000000
+
+	dc.b %00111100
+	dc.b %01100110
+	dc.b %01100110
+	dc.b %00111110
+	dc.b %00000110
+	dc.b %01100110
+	dc.b %00111100
+	dc.b %00000000
